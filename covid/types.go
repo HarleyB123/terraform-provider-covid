26a83cda4e96f9d62ad67236971860b7df020d00
Harley Bates <HarleyBatesOfficial@yahoo.co.uk>
2020-11-10 16:33:42 +0000

    Update types.go

--- covid/types.go
+++ covid/types.go
@@ -1,6 +1,5 @@
 package covid
 
-// Mcbroken holds ice cream machine failure rates in all avaailable cities
 type covid struct {
 	Countries   string  `json:"country"`
 	Cases float64 `json:"cases"`
